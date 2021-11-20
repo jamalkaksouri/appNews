29ae4096fcaa6b02ddae81062059d1d3fb809687
jamalkaksouri <jamal.kaksouri@gmail.com>
2021-11-20 22:32:02 +0330

    all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import AppNavbar from "./components/App Header/AppNavbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import AppCarousel from "./components/App Header/AppCarousel";
+import AppFooter from "./components/App Footer/AppFooter";
+import HomeBody from "./components/AppBody/Home/HomeBody";
+import NewsList from "./components/AppBody/News/NewsList";
+import { Route, Routes } from "react-router-dom";
+import AppContact from "./components/AppBody/ContactUs/AppContact";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      {/* <AppNavbar /> */}
+      {/* <AppCarousel />
+      <HomeBody /> */}
+      {/* <NewsList />
+      <AppFooter /> */}
+
+      <AppNavbar />
+      <Routes>
+        <Route exact path="/" element={<HomeBody />} />
+        <Route path="/NewsList" element={<NewsList />} />
+        <Route path="/AppContact" element={<AppContact />} />
+      </Routes>
+      {/* <HomeBody /> */}
+      {/* <NewsList /> */}
+      <AppFooter />
     </div>
   );
 }
